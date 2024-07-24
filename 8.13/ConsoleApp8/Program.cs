9d22c5f29f0fc14b672728209a498ad1bb87505d
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-07-24 20:36:15 -0300

    Add files via upload

---
+++ 8.13/ConsoleApp8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += i;
+            }
+
+            Console.WriteLine($"A soma dos números inteiros positivos de 1 a 100 é: {soma}");
+        }
+
+    }
+}
