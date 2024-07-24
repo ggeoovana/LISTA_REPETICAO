8560fd550eb99a7feafacf6c66cd573816a943d9
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-07-24 20:30:08 -0300

    Add files via upload

---
+++ 2.13/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Valor1;
+            int Valor2;
+
+            Console.WriteLine("Digite o primeiro valor:");
+            Valor1 = int.Parse(Console.ReadLine());
+
+            do
+            {
+
+                Console.WriteLine("Digite o segundo valor:");
+                Valor2 = int.Parse(Console.ReadLine());
+
+                if (Valor2 <= Valor1)
+                {
+                    Console.WriteLine("O segundo valor deve ser maior que o primeiro. Tente novamente.");
+                }
+
+            } while (Valor2 <= Valor1);
+
+            // Aqui, segundoValor é garantidamente maior que primeiroValor
+            Console.WriteLine($"Você digitou corretamente os valores: {Valor1} e {Valor2}");
+        }
+    }
+}
---
+++ 3.13/ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexo = string.Empty;
+
+            while (sexo != "F" && sexo != "M")
+            {
+                Console.Write("Por favor, insira o sexo (F ou M): ");
+                sexo = Console.ReadLine().ToUpper();
+
+                if (sexo != "F" && sexo != "M")
+                {
+                    Console.WriteLine("Entrada inválida. Tente novamente.");
+                }
+            }
+
+            Console.WriteLine("Sexo inserido: " + sexo);
+        }
+    }
+}
